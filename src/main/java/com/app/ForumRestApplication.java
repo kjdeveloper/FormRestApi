ea9c6784343b3353ee82f9e59a68ac74c83c13b6
kjdeveloper <kjdeveloper247@gmail.com>
2020-01-04 15:56:29 +0100

    v02 - created exception and repository

---
+++ src/main/java/com/app/ForumRestApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumRestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/app/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.app.repository;
+
+import com.app.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
